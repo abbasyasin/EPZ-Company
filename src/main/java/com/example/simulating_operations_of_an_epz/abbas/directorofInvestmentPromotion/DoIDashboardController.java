52b7c4aa061a096f373802733dc973bf8b801fa6
abbasyasin <abbasyasin1n2@gmail.com>
2024-11-25 00:32:36 +0600

    newscene

--- src/main/java/com/example/simulating_operations_of_an_epz/abbas/directorofInvestmentPromotion/DoIDashboardController.java
+++ src/main/java/com/example/simulating_operations_of_an_epz/abbas/directorofInvestmentPromotion/DoIDashboardController.java
@@ -1,7 +1,28 @@
 package com.example.simulating_operations_of_an_epz.abbas.directorofInvestmentPromotion;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class DoIDashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void logoutOnActionbutton(ActionEvent event) throws IOException {
+        Parent scene2Parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/simulating_operations_of_an_epz/login/LoginScene.fxml")));
+        Scene scene2 = new Scene(scene2Parent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setTitle("Login Page");
+        window.setScene(scene2);
+        window.show();
+    }
+}
