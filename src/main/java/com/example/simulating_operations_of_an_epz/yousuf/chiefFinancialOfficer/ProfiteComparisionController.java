483034ace939d822cae2876f8afabb4c748108f0
abbasyasin <abbasyasin1n2@gmail.com>
2024-12-12 23:57:39 +0600

    Yousufchnages

--- src/main/java/com/example/simulating_operations_of_an_epz/yousuf/chiefFinancialOfficer/ProfiteComparisionController.java
+++ src/main/java/com/example/simulating_operations_of_an_epz/yousuf/chiefFinancialOfficer/ProfiteComparisionController.java
@@ -1,20 +1,40 @@
 package com.example.simulating_operations_of_an_epz.yousuf.chiefFinancialOfficer;
 
+import com.example.simulating_operations_of_an_epz.abbas.executiveChairman.OperationalMetrics;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class ProfiteComparisionController {
 
     @FXML
-    private Button addbutton;
+    private NumberAxis yAxis;
+    @FXML
+    private CategoryAxis xAxis;
+    @FXML
+    private BarChart<String,Number> BudgetChart;
+    XYChart.Series<String,Number> ProfitSeries;
 
     @FXML
-    private TextField profitTextfield;
+    public void initialize() {
+        xAxis.setLabel("Date");
+        yAxis.setLabel("Value");
+        ProfitSeries = new XYChart.Series<>();
+        ProfitSeries.setName("Budget");
+        BudgetChart.getData().add(ProfitSeries);
+        BudgetChart.setVisible(false);
 
-    @FXML
-    private TextField yeartextField;
+
+    }
 
     @FXML
     void backbutton(ActionEvent event) {
@@ -23,6 +43,39 @@ public class ProfiteComparisionController {
 
     @FXML
     void showPiechart(ActionEvent event) {
+        BudgetChart.setVisible(true);
+        loadDepartmentBudget();
+
+    }
+
+    private void budgetChartValues(DepartmentBudget o){
+        ProfitSeries.getData().add(new XYChart.Data<>(o.getDate().toString(),o.getAmount()));
+        BudgetChart.getData().remove(ProfitSeries);
+        BudgetChart.getData().add(ProfitSeries);
+
+
+    }
+
+    public void loadDepartmentBudget(){
+        ObjectInputStream ois = null;
+        try {
+            DepartmentBudget y;
+            ois = new ObjectInputStream(new FileInputStream("departmentBudget.bin"));
+            while (true){
+                y= (DepartmentBudget) ois.readObject();
+                budgetChartValues(y);
+            }
+        }catch (Exception ex){
+            try{
+                if(ois != null){
+                    ois.close();
+                }
+            }catch(IOException ex2){
+                ex2.printStackTrace();
+            }
+            ex.printStackTrace();
+        }
+
 
     }
 
